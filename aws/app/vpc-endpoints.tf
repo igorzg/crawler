1acf54a46dc1b3a2c986be929bc33fc7ca8158f4
igorzg <igor.zg1987@gmail.com>
2019-02-27 13:21:01 +0100

    remove route table assoc on public subnets

--- aws/app/vpc-endpoints.tf
+++ aws/app/vpc-endpoints.tf
@@ -23,18 +23,3 @@ resource "aws_vpc_endpoint_subnet_association" "execute-api-private-az-1c" {
   vpc_endpoint_id = "${aws_vpc_endpoint.execute-api.id}"
   subnet_id       = "${aws_subnet.main-private-az-1c.id}"
 }
-
-resource "aws_vpc_endpoint_subnet_association" "execute-api-public-az-1a" {
-  vpc_endpoint_id = "${aws_vpc_endpoint.execute-api.id}"
-  subnet_id       = "${aws_subnet.main-public-az-1a.id}"
-}
-
-resource "aws_vpc_endpoint_subnet_association" "execute-api-public-az-1b" {
-  vpc_endpoint_id = "${aws_vpc_endpoint.execute-api.id}"
-  subnet_id       = "${aws_subnet.main-public-az-1b.id}"
-}
-
-resource "aws_vpc_endpoint_subnet_association" "execute-api-public-az-1c" {
-  vpc_endpoint_id = "${aws_vpc_endpoint.execute-api.id}"
-  subnet_id       = "${aws_subnet.main-public-az-1c.id}"
-}
